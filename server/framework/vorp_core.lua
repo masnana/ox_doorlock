997a9b4bd6052c7285653cd9511e687d36f2a298
masnana <hariyadinana87@gmail.com>
2024-10-31 03:17:14 +0800

    feat: vorp job check

--- server/framework/vorp_core.lua
+++ server/framework/vorp_core.lua
@@ -10,5 +10,28 @@ function GetCharacterId(player)
 end
 
 function IsPlayerInGroup(player, filter)
-    return player.job
+    local type = type(filter)
+
+    if type == 'string' then
+        if player.job == filter then
+            return player.job, player.jobGrade
+        end
+    else
+        local tabletype = table.type(filter)
+
+        if tabletype == 'hash' then
+            local grade = filter[data.name]
+            if grade and grade <= player.jobGrade then
+                return player.job, player.jobGrade
+            end
+        elseif tabletype == 'array' then
+            for i = 1, #filter do
+                local group = filter[i]
+
+                if player.job == group then
+                    return player.job, player.jobGrade
+                end
+            end
+        end
+    end
 end
